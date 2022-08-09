1517dc2c3e047253b12679bd5bc43e588413cf6b
nicolas bertagnolli <nmbertagnolli@gmail.com>
2022-08-08 22:10:57 -0600

    trying stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/dash
-set -eax
-
-output=`python -m pydocstyle --version`
-python -m pydocstyle "$1"
+output=`python -m pydocstyle "$1"`
+status=$?
 echo "::set-output name=output::$output"
+echo "::set-output name=status::$status"
